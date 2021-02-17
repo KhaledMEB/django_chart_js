9d7ad4fb664fb2628a49629be92d5796ae262c44
Khaled Mebhah <gk_mebhah@esi.dz>
2021-02-17 19:47:13 +0000

    Edit Club Model

--- DjangoChartJS/clubs/models.py
+++ DjangoChartJS/clubs/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Club(models.Model):
-    name = models.CharField(max_length = 200),
+    name = models.CharField(),
     money = models.IntegerField()
 
     def __str__(self):
-        return "{}-{}".format(self.name, self.money)
+        return "{} - {}".format(self.name, self.money)
