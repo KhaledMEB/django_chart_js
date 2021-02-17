e6a1541cf5e80f70dd4f9ac86e2d366a8058c885
Khaled Mebhah <gk_mebhah@esi.dz>
2021-02-17 20:19:24 +0000

    Add a Club Chart View and template

--- DjangoChartJS/clubs/views.py
+++ DjangoChartJS/clubs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Club
 
-# Create your views here.
+class ClubChartView(TemplateView):
+    template_name = 'clubs/chart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['qs'] = Club.objects.all()
+        return context
