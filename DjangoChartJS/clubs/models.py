760b447b9b07fd451afcad30a351a7919d86ec94
Khaled Mebhah <gk_mebhah@esi.dz>
2021-02-17 20:12:37 +0000

    Edit the Club model

--- DjangoChartJS/clubs/models.py
+++ DjangoChartJS/clubs/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Club(models.Model):
-    name = models.CharField(),
+    name = models.CharField(max_length=60, default='London')
+    city = models.CharField(max_length=60, default='London')
     money = models.IntegerField()
 
     def __str__(self):
-        return "{} - {}".format(self.name, self.money)
+        return f"{self.name} {self.city} {self.money}"
